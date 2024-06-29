65f1671ba4bff1e09d2fd9e7f6c320af442b2d0e
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-06-29 03:20:48 -0400

    fixed defines clashing with htonll macros (temporary)

--- mira_server/client.c
+++ mira_server/client.c
@@ -10,8 +10,10 @@
 #include "protocol.h"
 #include "utils.h"
 
-#define htonll(x) ((1 == htonl(1)) ? (x) : ((uint64_t) htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
-#define ntohll(x) ((1 == ntohl(1)) ? (x) : ((uint64_t) ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#ifndef __APPLE__
+	#define htonll(x) ((1 == htonl(1)) ? (x) : ((uint64_t) htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+	#define ntohll(x) ((1 == ntohl(1)) ? (x) : ((uint64_t) ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#endif
 
 #define WORKER_EXIT do { \
 	params->thread->completed = true; \
